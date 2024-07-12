17ecd9c0cdb7301f2eb7a699cfba2b9c384fabf6
Evian <allainevan1@gmail.com>
2024-07-12 08:55:52 +0200

    Follow and block user

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,5 +1,6 @@
 import Post from "./Post"
 
+
 const Feed = () => {
     return (
         <div className="p-4 bg-white shadow-md rounded-lg flex flex-col gap-12">
--- src/components/leftMenu/ProfileCard.tsx
+++ src/components/leftMenu/ProfileCard.tsx
@@ -1,15 +1,39 @@
+import prisma from "@/lib/client"
+import { auth } from "@clerk/nextjs/server"
 import Image from "next/image"
 
-const ProfileCard = () => {
+const ProfileCard = async () => {
+
+    const {userId} = auth()
+
+    if (!userId) return null;
+
+    const user = await prisma.user.findFirst({
+        where:{
+            id:userId,
+        },
+        include:{
+            _count:{
+                select:{
+                    followers:true
+                }
+            }
+        }
+    })
+    console.log(user)
+
+    if (!user) return null;
+
+
     return (
         <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
             <div className="h-20 relative">
-                <Image src="https://images.pexels.com/photos/25785552/pexels-photo-25785552/free-photo-of-la-nuit.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                <Image src={user.cover || "/noCover.png"}
                 alt=""
                 fill
                 className="rounded-md object-cover" 
                 />
-                <Image src="https://images.pexels.com/photos/13943109/pexels-photo-13943109.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                <Image src={user.avatar || "/noAvatar.png"}
                 alt=""
                 width={48}
                 height={48}
@@ -17,7 +41,7 @@ const ProfileCard = () => {
                 />
             </div>
             <div className="h-20 flex flex-col gap-2 items-center">
-                <span className="font-semibold">Edward Gabriel May</span>
+                <span className="font-semibold">{(user.name && user.surname) ? user.name + " " + user.surname : user.username}</span>
                 <div className="flex items-center gap-4">
                     <div className="flex">
                     <Image src="https://images.pexels.com/photos/13943109/pexels-photo-13943109.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
@@ -39,7 +63,7 @@ const ProfileCard = () => {
                 className="rounded-full object-cover w-3 h-3" 
                 />
                     </div>
-                    <span className="text-xs text-gray-500">500 Followers</span>
+                    <span className="text-xs text-gray-500">{user._count.followers} Followers</span>
                 </div>
                 <button className="bg-blue-500 text-white text-xs p-2 rounded-md">My Profile</button>
             </div>
