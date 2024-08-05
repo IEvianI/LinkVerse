f61ccaa6f81499d9ad00d2b5fbbe8d47b7ae040d
Evian <allainevan1@gmail.com>
2024-08-05 14:06:26 +0200

    Reupdate PostInteraction

--- src/components/feed/PostInteraction.tsx
+++ src/components/feed/PostInteraction.tsx
@@ -40,6 +40,7 @@ const PostInteraction = ({
             console.log(err)
         }
     }
+    {/* */}
     return (
         <div className="flex items-center justify-between text-sm my-4">
                 <div className="flex gap-8">
