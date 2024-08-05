921e7fa282df0c075f3b56d71e6b2df12f289125
Evian <allainevan1@gmail.com>
2024-08-05 14:39:04 +0200

    vercel_test_3

--- src/components/feed/PostInteraction.tsx
+++ src/components/feed/PostInteraction.tsx
@@ -40,7 +40,7 @@ const PostInteraction = ({
             console.log(err)
         }
     }
-    {/* Post I */}
+    {/* Post */}
     return (
         <div className="flex items-center justify-between text-sm my-4">
                 <div className="flex gap-8">
