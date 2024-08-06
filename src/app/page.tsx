b6a24214ee6fad59db23840f2f3f9e082649039b
Evian <allainevan1@gmail.com>
2024-08-06 15:07:32 +0200

    update mobile homepage friendrequests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Stories from "@/components/Stories"
 
 const Homepage = () => {
   return (
-    <div className='flex gap-6 pt-6'>
+    <div className='flex max-[767px]:flex-col gap-6 pt-6'>
       <div className="hidden xl:block w-[20%]"><LeftMenu type="home" /></div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
@@ -15,7 +15,7 @@ const Homepage = () => {
           <Feed />
         </div>
       </div>
-      <div className="hidden lg:block w-[30%]"><RightMenu /></div>
+      <div className="max-[767px]:w-full lg:block w-[30%]"><RightMenu /></div>
     </div>
   )
 }
